6f32209c7c707a8e76ee2dca2fbf9cde9d9368e5
AlenaYashkina <120055830+AlenaYashkina@users.noreply.github.com>
2023-01-02 20:23:20 +0100

    First

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,28 @@
+package koschei;
+
+import koschei.models.Ocean1;
+import koschei.models.Rabbit4;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+    private Rabbit4 rabbit;
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+
+    @Autowired
+    public void setRabbit(Rabbit4 rabbit) {
+        this.rabbit = rabbit;
+    }
+}
